e8fcc6519a0eb10b9b77cf3c85c7624929d8ee9b
John Theo <johntheo@skyscanner.net>
2024-08-28 16:12:00 +0200

    minor fixing

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,11 @@ import ReactGA from 'react-ga4';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactGA.initialize('G-PV21BW267L');
+// ReactGA.initialize("G-PV21BW267L", {
+//     gtagOptions: {
+//       debug_mode: true, // This enables debug mode
+//     },
+//   });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
